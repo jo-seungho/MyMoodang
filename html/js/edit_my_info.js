b3b89b16e22a2f4967bc96ccf6b0ee2a190df998
김서영 <ksy0914@gmail.com>
2023-04-12 18:36:57 +0900

    마이페이지 화면 구현된 것들과 사이드바 메뉴 경로 연결 / 정보수정 모달창 완성

--- html/js/edit_my_info.js
+++ html/js/edit_my_info.js
@@ -9,3 +9,50 @@ function validatePwd() {
     }
 }
 
+
+// Get the modal
+var modal = document.getElementById("updatePwdForm");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Get the modal
+var modal = document.getElementById("deleteForm");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
